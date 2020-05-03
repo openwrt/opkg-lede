openwrt/opkg-lede

commit 71f02a3eb65b2996957faf81c2e54196a35f3c59

libopkg: add support for signature checking through usign
Adds a new configure switch "--enable-usign" which enables code to perform
package feed signature checking using an external "/usr/sbin/opkg-key" helper
program.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

libopkg/opkg_download.c
@@ -19,6 +19,7 @@
 
 #include "config.h"
 
+#include <sys/wait.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <libgen.h>
@@ -342,7 +343,28 @@ opkg_prepare_url_for_install(const char *url, char **namep)
 int
 opkg_verify_file (char *text_file, char *sig_file)
 {
-#if defined HAVE_GPGME
+#if defined HAVE_USIGN
+	int status = -1;
+	int pid;
+
+    if (conf->check_signature == 0 )
+        return 0;
+
+	pid = fork();
+	if (pid < 0)
+		return -1;
+
+	if (!pid) {
+		execl("/usr/sbin/opkg-key", "opkg-key", "verify", sig_file, text_file, NULL);
+		exit(255);
+	}
+
+	waitpid(pid, &status, 0);
+	if (!WIFEXITED(status) || WEXITSTATUS(status))
+		return -1;
+
+	return 0;
+#elif defined HAVE_GPGME
     if (conf->check_signature == 0 )
         return 0;
     int status = -1;

configure.ac
@@ -169,6 +169,15 @@ if test "x$want_gpgme" = "xyes"; then
   fi
 fi
 
+AC_ARG_ENABLE(usign,
+    AC_HELP_STRING([--enable-usign], [Enable signature checking with usign
+      [[default=yes]] ]),
+    [want_usign="$enableval"], [want_usign="yes"])
+
+if test "x$want_usign" = "xyes"; then
+  AC_DEFINE(HAVE_USIGN, 1, [Define if you want usign support])
+fi
+
 AC_SUBST(GPGME_CFLAGS)
 AC_SUBST(GPGME_LIBS)
 

libopkg/opkg.c
@@ -599,7 +599,7 @@ opkg_update_package_lists(opkg_progress_callback_t progress_callback,
 		}
 		free(url);
 
-#if defined(HAVE_GPGME) || defined(HAVE_OPENSSL)
+#if defined(HAVE_GPGME) || defined(HAVE_OPENSSL) || defined(HAVE_USIGN)
 		if (conf->check_signature) {
 			char *sig_file_name;
 			/* download detached signitures to verify the package lists */

libopkg/opkg_cmd.c
@@ -169,7 +169,7 @@ opkg_update_cmd(int argc, char **argv)
 			    list_file_name);
 	  }
 	  free(url);
-#if defined(HAVE_GPGME) || defined(HAVE_OPENSSL)
+#if defined(HAVE_GPGME) || defined(HAVE_OPENSSL) || defined(HAVE_USIGN)
           if (conf->check_signature) {
               /* download detached signitures to verify the package lists */
               /* get the url for the sig file */

libopkg/opkg_install.c
@@ -1288,7 +1288,7 @@ opkg_install_pkg(pkg_t *pkg, int from_upgrade)
      }
 
      /* check that the repository is valid */
-     #if defined(HAVE_GPGME) || defined(HAVE_OPENSSL)
+     #if defined(HAVE_GPGME) || defined(HAVE_OPENSSL) || defined(HAVE_USIGN)
      char *list_file_name, *sig_file_name, *lists_dir;
 
      /* check to ensure the package has come from a repository */

commit b6f196764a30e75f4667ac68ad92c001dce19e91

libopkg: use xsystem() to spawn opkg-key
Instead of the custom fork()/exec() implementation, use the existing
xsystem() helper function which provides a number of benefits:

 - It readily provides error reporting in case the execution fails
 - It has a simpler api
 - It uses vfork() internally which avoids the need to copy pages

This likely fixes https://bugs.openwrt.org/index.php?do=details&task_id=2734.

Fixes: 71f02a3 ("libopkg: add support for signature checking through usign")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

libopkg/opkg_download.c
@@ -17,7 +17,6 @@
    General Public License for more details.
 */
 
-#include <sys/wait.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <libgen.h>
@@ -299,29 +298,10 @@ int opkg_prepare_url_for_install(const char *url, char **namep)
 int opkg_verify_file(char *text_file, char *sig_file)
 {
 #if defined HAVE_USIGN
-	int status = -1;
-	int pid;
+	const char *argv[] = { "/usr/sbin/opkg-key", "verify", sig_file,
+	                       text_file, NULL };
 
-	if (conf->check_signature == 0)
-		return 0;
-
-	pid = fork();
-	if (pid < 0) {
-		opkg_perror(ERROR, "Failed to fork opkg-key process");
-		return -1;
-	}
-
-	if (!pid) {
-		execl("/usr/sbin/opkg-key", "opkg-key", "verify", sig_file,
-		      text_file, NULL);
-		exit(255);
-	}
-
-	waitpid(pid, &status, 0);
-	if (!WIFEXITED(status) || WEXITSTATUS(status))
-		return -1;
-
-	return 0;
+	return xsystem(argv) ? -1 : 0;
 #else
 	/* mute `unused variable' warnings. */
 	(void)sig_file;