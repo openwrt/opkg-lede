openwrt/opkg-lede

commit 532f92debcc24fae94246f9a40ef8aca7547930c

libopkg: tighten length of fields in struct abstract_pkg
Should reduce the size of that struct and hence memory consumption
during dependency resolution (48 bytes vs. 56 bytes on 64-bit).

Suggested-by: Jo-Philipp Wich <jo@mein.io>
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

libopkg/pkg.h
@@ -105,17 +105,17 @@ enum pkg_fields {
 
 struct abstract_pkg {
 	char *name;
-	int dependencies_checked;
 	pkg_vec_t *pkgs;
-	pkg_state_status_t state_status;
-	pkg_state_flag_t state_flag;
+	int dependencies_checked:1;
+	pkg_state_status_t state_status:4;
+	pkg_state_flag_t state_flag:11;
 
 	/* XXX: This should be abstract_pkg_vec_t for consistency. */
 	struct abstract_pkg **depended_upon_by;
 
 	abstract_pkg_vec_t *provided_by;
 	abstract_pkg_vec_t *replaced_by;
-};
+} __attribute__((__packed__)) ;
 
 #include "pkg_depends.h"
 

commit 11ee06ea67ebe6a6f11323ad5a6d3c77071488cd

libopkg: fix removal of packages
The 'dependencies_checked' field in struct abstract_pkg is also used
when removing packages, and in that case it is set to '2'.
Make the field wide enough to be able to contain that value.

Fixes: 532f92d ("libopkg: tighten length of fields in struct abstract_pkg")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

libopkg/pkg.h
@@ -113,7 +113,7 @@ struct abstract_pkg {
 	abstract_pkg_vec_t *provided_by;
 	abstract_pkg_vec_t *replaced_by;
 
-	int dependencies_checked:1;
+	int dependencies_checked:2;
 	pkg_state_status_t state_status:4;
 	pkg_state_flag_t state_flag:11;
 };